b51f9a28b75a89ef8f7ac31e361f4668c985b949
kenway214 <kenway214@outlook.com>
2023-09-22 00:28:45 +0530

    marble: Add back some Media packages
    09-14 23:05:45.835   791  2853 W libc    : Unable to set property "ctl.interface_start" to "android.hardware.media.c2@1.0::IComponentStore/dolby": error code: 0x20
    09-14 23:05:45.835   791  2853 I hwservicemanager: Tried to start android.hardware.media.c2@1.0::IComponentStore/dolby as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -298,13 +298,17 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.2.vendor \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
+    libavservices_minijail_vendor \
+    libcodec2_hidl@1.0.vendor \
+    libcodec2_hidl@1.1.vendor \
+    libcodec2_hidl@1.2.vendor \
+    libcodec2_soft_common.vendor \
+    libcodec2_vndk.vendor \
     libminijail \
     libminijail.vendor \
-    libavservices_minijail \
-    libcodec2_hidl@1.1.vendor \
-    libavservices_minijail_vendor \
-    libavservices_minijail.vendor \
-    libcodec2_soft_common.vendor \
     libsfplugin_ccodec_utils.vendor
 
 PRODUCT_COPY_FILES += \
