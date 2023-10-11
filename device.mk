3e448c9d238b0eeb321c57a9d2b80cdc610d0b23
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-11 16:16:57 +0800

    marble: import scripts from stock
    * Refer to https://github.com/cupid-development/android_device_xiaomi_sm8450-common/tree/lineage-20/rootdir

--- device.mk
+++ device.mk
@@ -75,6 +75,21 @@ PRODUCT_PACKAGES += \
     QtiTelephony \
     qti-telephony-common
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    charger_fw_fstab.qti \
+    fstab.qcom \
+    init.class_main.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.rc \
+    init.qcom.sh \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    ueventd.qcom.rc \
+    ueventd-odm.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
