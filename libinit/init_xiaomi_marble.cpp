marble-development/android_device_xiaomi_marble_legacy

marble: Update IN fingerprint prop
Update from MIUI V14.0.5.0.TMRINXM

libinit/init_xiaomi_marble.cpp
@@ -12,7 +12,7 @@
 
 #define FINGERPRINT_GL "POCO/marble_global/marble:13/SKQ1.221022.001/V14.0.6.0.TMRMIXM:user/release-keys"
 #define FINGERPRINT_CN "Redmi/marble/marble:13/SKQ1.221022.001/V14.0.23.0.TMRCNXM:user/release-keys"
-#define FINGERPRINT_IN "POCO/marblein/marblein:13/SKQ1.221022.001/V14.0.1.0.TMRMIXM:user/release-keys"
+#define FINGERPRINT_IN "POCO/marblein/marblein:13/SKQ1.221022.001/V14.0.5.0.TMRINXM:user/release-keys"
 
 static const variant_info_t marble_global_info = {
     .hwc_value = "GL",

marble-development/android_device_xiaomi_marble

libinit/init_xiaomi_marble.cpp
@@ -12,7 +12,7 @@
 
 #define FINGERPRINT_GL "POCO/marble_global/marble:13/SKQ1.221022.001/V14.0.7.0.TMRMIXM:user/release-keys"
 #define FINGERPRINT_CN "Redmi/marble/marble:13/SKQ1.221022.001/V14.0.23.0.TMRCNXM:user/release-keys"
-#define FINGERPRINT_IN "POCO/marblein/marblein:13/SKQ1.221022.001/V14.0.1.0.TMRMIXM:user/release-keys"
+#define FINGERPRINT_IN "POCO/marblein/marblein:13/SKQ1.221022.001/V14.0.5.0.TMRMIXM:user/release-keys"
 
 static const variant_info_t marble_global_info = {
     .hwc_value = "GL",