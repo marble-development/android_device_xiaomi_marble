207f9b42deebf7c8a3a43a64657bfffeb853b44f
saikiran2001 <bjsaikiran@gmail.com>
2023-10-20 18:50:13 +0200

    marble: Build required packages required by vendor.xiaomi.sensor.citsensorservice@2.0-impl

--- device.mk
+++ device.mk
@@ -136,6 +136,9 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@4.0-impl \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh \
@@ -147,10 +150,15 @@ PRODUCT_PACKAGES += \
     libsdedrm \
     libsdmcore \
     libsdmutils \
+    libtinyxml \
+    libtinyxml2 \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.allocator@3.0.vendor \
+    vendor.qti.hardware.display.allocator@4.0.vendor \
     vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer@3.0.vendor \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.demura-service \
     vendor.qti.hardware.display.config-V1-ndk.vendor \
@@ -163,6 +171,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.1.vendor \
     vendor.qti.hardware.display.mapperextensions@1.2.vendor
 
 PRODUCT_COPY_FILES += \
